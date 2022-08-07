29a17bd4848f13cdcca434783699d3309ecd5014
Arianna <stanlyfiji@gmail.com>
2022-08-07 09:26:31 +0300

    Huray to Masha

---
+++ Example4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Цhat is your name?");
+string userName=Console.ReadLine();
+if (userName.ToLower()=="маша")
+Console.WriteLine("Ура, это же Маша!");
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(userName);
+}
+
